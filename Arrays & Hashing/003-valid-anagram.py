bc2d0abf1c6ff27277414ed4a0fe6ec803cfc290
Vishal Vivek <vishalvivek14332@gmail.com>
2026-06-27 20:15:12 +0530

    Solve: Arrays & Hashing - Valid Anagram Differntly

--- Arrays & Hashing/003-valid-anagram.py
+++ Arrays & Hashing/003-valid-anagram.py
@@ -2,12 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        count_s = {}
-        count_t = {}
+        countS = {}
+        countT = {}
         for i in range(len(s)):
-            count_s[s[i]] = count_s.get(s[i], 0) + 1
-            count_t[t[i]] = count_t.get(t[i], 0) + 1
-        for key, val in count_s.items():
-            if not (key in count_t and count_t[key] == val):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
                 return False
         return True
